ccc808077e743cd947735755ad3ff415d2e3a5aa
frexor1 <frexorplay@gmail.com>
2023-06-05 21:36:21 +0200

    many wygrywa drania roku123

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.logging.SocketHandler;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        ZAWODNIK z1 = new ZAWODNIK("Maciuś2115", "R", 13);
+        ZAWODNIK z1 = new ZAWODNIK("Maciuś2115dran", "R", 13);
 
         ZAWODNIK z2 = new ZAWODNIK("Ulcia", "R", 4);
         ZAWODNIK z3 = new ZAWODNIK("Domiś", "Ż", 23);
